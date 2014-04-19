3a78724d166094706266572078a619001863119d
Matej Urbas <matej.urbas@gmail.com>
2014-04-19 21:21:47 +0100

    Added the entity manager to PlessJpaController.

--- app/si/urbas/pless/PlessJpaController.java
+++ app/si/urbas/pless/PlessJpaController.java
@@ -3,9 +3,15 @@ package si.urbas.pless;
 import si.urbas.pless.db.TransactionCallback;
 import si.urbas.pless.db.TransactionFunction;
 
+import javax.persistence.EntityManager;
+
 import static si.urbas.pless.db.PlessJpaTransactions.getJpaTransactions;
 
 public class PlessJpaController extends PlessController {
+
+  protected static EntityManager em() {
+    return getJpaTransactions().getEntityManager();
+  }
 
   protected static void withTransaction(TransactionCallback callback) {
     getJpaTransactions().withTransaction(callback);
