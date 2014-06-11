aa37fc0564b9df832b5c9a01b968fea063fdf36f
Matej Urbas <matej.urbas@gmail.com>
2014-06-11 21:29:50 +0100

    Moved the test email content from the old test signup servcie to the new test user account service.

--- plessTest/app/si/urbas/pless/test/users/TestUserAccountService.java
+++ plessTest/app/si/urbas/pless/test/users/TestUserAccountService.java
@@ -1,9 +1,15 @@
 package si.urbas.pless.test.users;
 
 import play.api.templates.Html;
+import si.urbas.pless.users.PlessUser;
 import si.urbas.pless.users.UserAccountService;
 
 public class TestUserAccountService extends UserAccountService {
+  @Override
+  public Html signupEmailContent(PlessUser userDetails) {
+    return new Html(new scala.collection.mutable.StringBuilder(userDetails.toString()));
+  }
+
   @Override
   public Html passwordResetEmailContent(String email, String resetCode) {
     return new Html(new scala.collection.mutable.StringBuilder("Password reset email: " + email + " :: reset code: " + resetCode));
