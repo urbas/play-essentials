5732dddc2377ef1428aede355563abb292f306e0
Matej Urbas <matej.urbas@gmail.com>
2014-07-14 09:12:24 +0100

    UserAccountService: documented the signup and reset password procedures.

--- pless/app/si/urbas/pless/users/UserAccountService.java
+++ pless/app/si/urbas/pless/users/UserAccountService.java
@@ -18,12 +18,38 @@ import static si.urbas.pless.util.ServiceLoader.createServiceLoader;
  * Responsible for actions related to user accounts. This service can be replaced by a custom one through
  * the configuration key {@link si.urbas.pless.users.UserAccountService#CONFIG_USER_ACCOUNT_SERVICE} (see the README
  * file for more detailed instructions).
+ * <p>
+ * <h2>Signup procedure</h2>
+ * <ul>
+ * <li>User calls {@link UserController#signUp()} with some multiform data (at least the email and password).</li>
+ * <li>{@link UserAccountService#getSignupForm()} is called to validate the user's data.</li>
+ * <li>If the form successfully validates user's data, then {@link si.urbas.pless.users.UserAccountService#createUser(play.data.Form)}
+ * is called, otherwise an error message is returned and the signup procedure ends here.</li>
+ * <li>If the user is successfully created, the method {@link si.urbas.pless.users.UserAccountService#afterUserPersisted(PlessUser)}
+ * is called.</li>
+ * <li>Finally, the method {@link si.urbas.pless.users.UserAccountService#sendSignupEmail(PlessUser)} is called.</li>
+ * </ul>
+ * <p>
+ * <h2>Password reset</h2>
+ * <ul>
+ * <li>User calls {@link si.urbas.pless.users.UserController#requestPasswordReset(String)}, which tries to find the
+ * user and, upon success, generates a password reset code for that user and calls
+ * {@link si.urbas.pless.users.UserAccountService#sendPasswordResetEmail(String, String)}.</li>
+ * <li>The user has to visit the {@link si.urbas.pless.users.UserController#resetPasswordForm(String, String)}
+ * page and must submit the new password with the correct reset code and email.</li>
+ * <li>If the user successfully reset the password, the method
+ * {@link si.urbas.pless.users.UserAccountService#sendPasswordResetConfirmationEmail(String)} is called.</li>
+ * </ul>
  */
 @PlessServiceConfigKey(UserAccountService.CONFIG_USER_ACCOUNT_SERVICE)
 public class UserAccountService implements PlessService {
 
   public static final String CONFIG_USER_ACCOUNT_SERVICE = "pless.userAccountService";
 
+  /**
+   * @return this form is used in the {@link UserController#signUp()} REST API. This form should validate that the user
+   * provided valid signup information. If the validation succeeded, the form
+   */
   public Form<?> getSignupForm() {return form(SignupData.class);}
 
   public PlessUser createUser(Form<?> signupForm) {
