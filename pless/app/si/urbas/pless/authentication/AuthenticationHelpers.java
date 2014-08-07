7ec33c407b0bb4885f5ec3c79a478dfbc7a66243
Matej Urbas <matej.urbas@gmail.com>
2014-08-07 19:07:30 +0100

    AuthenticationHelpers: generalized a type of a field.

--- pless/app/si/urbas/pless/authentication/AuthenticationHelpers.java
+++ pless/app/si/urbas/pless/authentication/AuthenticationHelpers.java
@@ -1,7 +1,6 @@
 package si.urbas.pless.authentication;
 
 import play.mvc.Result;
-import play.mvc.Results;
 import si.urbas.pless.util.ApiResults;
 
 import java.util.function.Function;
@@ -13,7 +12,7 @@ import static si.urbas.pless.authentication.LoginService.loginService;
 public class AuthenticationHelpers {
 
   public static final String ERROR_MESSAGE_USER_NOT_LOGGED_IN = "Could not process request. Authentication required.";
-  public static final Results.Status USER_NOT_LOGGED_IN_RESULT = ApiResults.error(ERROR_MESSAGE_USER_NOT_LOGGED_IN);
+  public static final Result USER_NOT_LOGGED_IN_RESULT = ApiResults.error(ERROR_MESSAGE_USER_NOT_LOGGED_IN);
 
   /**
    * @param actionBody this callback will be called if a user is logged in.
@@ -26,6 +25,7 @@ public class AuthenticationHelpers {
 
   /**
    * This function is intended for REST APIs.
+   *
    * @param actionBody this callback will be called if a user is logged in.
    * @return the result of the given callback or a standard JSON error response (with a message explaining that the user
    * was not authenticated).
