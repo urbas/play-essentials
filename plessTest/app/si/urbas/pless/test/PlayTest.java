0d386351b1bac9c08c4ba553e8db2ff190afbad9
Matej Urbas <matej.urbas@gmail.com>
2014-08-14 09:03:10 +0100

    PlayTest: Added the `configureService` helper method that adds a configuration entry to the test play configuration.

--- plessTest/app/si/urbas/pless/test/PlayTest.java
+++ plessTest/app/si/urbas/pless/test/PlayTest.java
@@ -1,7 +1,19 @@
 package si.urbas.pless.test;
 
+import si.urbas.pless.PlessService;
+
 import java.util.HashMap;
+
+import static si.urbas.pless.util.ServiceLoader.getServiceConfigKey;
 
 public interface PlayTest {
   HashMap<String, String> getPlayApplicationConfiguration();
+
+  public static void configureService(HashMap<String, String> playApplicationConfiguration,
+                                      Class<? extends PlessService> plessServiceClass) {
+    playApplicationConfiguration.put(
+      getServiceConfigKey(plessServiceClass),
+      plessServiceClass.getCanonicalName()
+    );
+  }
 }
